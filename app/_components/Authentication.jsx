8b155d3190d816b073624295bb7cddda8997f3b1
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-17 22:05:09 +0530

    after sign-in : redirect to dashboard

--- app/_components/Authentication.jsx
+++ app/_components/Authentication.jsx
@@ -3,18 +3,17 @@ import { auth } from '@/configs/firebaseConfig';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react'
 import toast from 'react-hot-toast';
-
+import { useRouter } from 'next/navigation';
 function Authentication({ children }) {
     const provider = new GoogleAuthProvider();
-
+    const router = useRouter();
     const onSignInClick = () => {
-        console.log("CLICKL")
         signInWithPopup(auth, provider)
             .then((result) => {
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
                 const user = result.user;
-                console.log(user)
+                router.push('/dashboard');
             }).catch((error) => {
                 toast.error("Error signing in with Google. Please try again after sometime.", {
                     style: {
