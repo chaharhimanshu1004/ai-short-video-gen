c40442248608552f873c3d29a72c7213d0daa4db
chaharhimanshu1004 <chaharhimanshu03@gmail.com>
2025-05-17 22:06:01 +0530

    added toast after signin

--- app/_components/Authentication.jsx
+++ app/_components/Authentication.jsx
@@ -13,6 +13,13 @@ function Authentication({ children }) {
                 const credential = GoogleAuthProvider.credentialFromResult(result);
                 const token = credential.accessToken;
                 const user = result.user;
+                toast.success("Successfully signed in with Google", {
+                    style: {
+                        background: '#1e1e1e',
+                        color: '#ffffff',
+                        border: '1px solid #333'
+                    },
+                });
                 router.push('/dashboard');
             }).catch((error) => {
                 toast.error("Error signing in with Google. Please try again after sometime.", {
